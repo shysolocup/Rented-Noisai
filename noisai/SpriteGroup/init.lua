e98893741944feb080754a6e846e6cbaabdda5b4
paige <88659700+paishee@users.noreply.github.com>
2023-11-27 13:53:40 -0600

    Update init.lua

--- noisai/SpriteGroup/init.lua
+++ noisai/SpriteGroup/init.lua
@@ -1,4 +1,10 @@
 local SpriteGroup = {}
 SpriteGroup.__index = SpriteGroup
 
+local functions = script.Parent.functions
+
+for _, f in pairs(functions:getChildren()) do
+  f(SpriteGroup)
+end
+
 return SpriteGroup
