c9c4a273533d575a34da27fb4ddbc6c26476fe23
paige <88659700+paishee@users.noreply.github.com>
2023-11-28 16:05:50 -0600

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,11 +1,10 @@
 local noiseMax = 100
-local noiseCurrent = 0
 local level = 10
 
 
 for i = 0, 9 do i = i + 1
     local rand = math.random(1, 20);
-    local chance = (rand - ( noiseMax/10 ) - (noiseCurrent/100) )
+    local chance = rand - ( noiseMax/10 )
     
     local move = chance <= level;
     
