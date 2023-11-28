633dc854445f33b6b43152429d763008fd344cb6
paige <88659700+paishee@users.noreply.github.com>
2023-11-28 12:49:57 -0600

    Update new.lua

--- noisai/Bot/functions/new.lua
+++ noisai/Bot/functions/new.lua
@@ -1,7 +1,23 @@
+local Signal = require(script.Parent.Parent.Parent.Signal);
+
+
 return (function(Bot)
         
-    function Bot.new()
-        local self = setmetatable( {}, Bot )
+    function Bot.new(Level: number, Vision)
+        local self = setmetatable( {
+
+            --[[ data ]]
+            Level = Level,
+            Vision = Vision,
+            Path = "placeholder",
+
+            --[[ signals ]]
+            Moving = Signal.new(),
+            Stuck = Signal.new(),
+            Active = Signal.new()
+            
+        }, Bot )
+            
         return self;
     end
         
