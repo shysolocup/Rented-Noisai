6775fc058eec729b2cb3a6b291df5b2e76fe2f70
paige <88659700+paishee@users.noreply.github.com>
2023-11-28 13:58:56 -0600

    Update new.lua

--- noisai/functions/new.lua
+++ noisai/functions/new.lua
@@ -1,14 +1,58 @@
+-- workspace setup
+--[[
+
+workspace [
+    Noisai: module { ... }
+
+    Character: folder [
+        Sprites: folder [
+            First: part { 
+                __sprite: decal/texture 
+            }
+
+            Second: part { 
+                __sprite: decal/texture 
+            }
+
+            Last: part { 
+                __sprite: decal/texture 
+            }
+        ]
+    ]
+]
+
+]]
+
+
+-- code example
+--[[
+
+local Noisai = require('workspace.Noisai');
+local data = workspace.Character
+
+
+Noisai.new(10, data, function(sprites)
+    return {
+        { 1, sprites.First },
+        { 2, sprites.Second },
+        { 3, sprites.Last }
+    }
+end);
+
+]]
+
+
 local Signal = require(script.Parent.Parent.Signal);
 
 
 return (function(Noisai)
         
-    function Noisai.new(Level: number, Vision)
+    function Noisai.new(Level: number, Data, callback)
         local self = setmetatable( {
 
             --[[ data ]]
             Level = Level,
-            Vision = Vision,
+            Data = Data
             Path = "placeholder",
 
             At = 1
