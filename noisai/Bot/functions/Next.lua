b9dea78967664b01ddfc9b376153c91904e59cb7
paige <88659700+paishee@users.noreply.github.com>
2023-11-28 12:56:45 -0600

    Create Next.lua

---
+++ noisai/Bot/functions/Next.lua
@@ -0,0 +1,11 @@
+local Signal = require(script.Parent.Parent.Parent.Signal);
+
+
+return (function(Bot)
+        
+    function Bot:Next()
+        return self:To(self.At+1)
+    end
+    
+        
+end)
