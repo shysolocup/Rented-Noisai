9dbb1efb8f0acff212bff83c05cda93b1c05d183
paige <88659700+paishee@users.noreply.github.com>
2023-11-28 13:29:11 -0600

    Update new.lua

--- noisai/Path/functions/new.lua
+++ noisai/Path/functions/new.lua
@@ -1,26 +1,32 @@
 --[[
 
 local Noisai = require(workspace.Noisai)
-local dir = workspace.sprites
+local sprites = workspace.sprites
 
 
-local path = Noisai.Path.new(function(sprites) 
-    return {
-        { sprites.start, 1 },
-        { sprites.end, 2 }
-    }
-end);
+local path = Noisai.Path.new({
+    // { Name: string, Index: number, Part: part, Sprite: decal/texture }
+
+    { "Start", 1, sprites.start, sprites.start.Sprite },
+    { "Mid", 2, sprites.mid, sprites.mid.Sprite },
+    { "End", 3, sprites.end, sprites.end.Sprite }
+});
 
 
 ]]
 
-return (function(Path) function Path.new(callback)
-    
-    local self = setmetatable( {
-            
-    }, Path )
+local PathElement = require(script.Parent.Parent.Parent.PathElement)
 
-        
+
+return (function(Path) function Path.new(obj)
+            
+    local self = setmetatable( {}, Path )
+
+    for _, elem in pairs(obj) do
+        local pe = PathElement.new(elem);
+        self[pe.Index] = pe;
+    end
+            
     return self;
             
 end end)
