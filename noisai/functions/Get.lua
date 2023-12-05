26ca5364daa2bd51c9d3c81b3f9e70bebdacc365
paige <88659700+paishee@users.noreply.github.com>
2023-12-05 10:27:33 -0600

    Update Get.lua

--- noisai/functions/Get.lua
+++ noisai/functions/Get.lua
@@ -1,8 +1,8 @@
 return (function(Noisai)
-        
-    function Noisai:Get(i)
-        return self.Path[i]
-    end
-    
-        
+
+	function Noisai:Get(i)
+		return self.Path[i]
+	end
+
+
 end)
