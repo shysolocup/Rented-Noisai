e886130e9751ebb98aa90738c677cdbf10235f3f
paige <88659700+paishee@users.noreply.github.com>
2023-12-05 13:13:18 -0600

    Update PlayVoice.lua

--- noisai/functions/PlayVoice.lua
+++ noisai/functions/PlayVoice.lua
@@ -1,8 +1,8 @@
 return (function(Noisai)
 
-	function Noisai:PlayVoice(v)
-		if not v then v = math.random(1, #self.Voicelines) end
-		self.Voicelines[v]:Play()
+	function Noisai:PlayVoice(i)
+		if not i then i = math.random(1, #self.Voicelines) end
+		self.Voicelines[i]:Play()
 	end
 
 end)
