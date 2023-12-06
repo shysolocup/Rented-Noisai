479bd58e9888f256286987bff4e8bc52897ff16c
paige <88659700+paishee@users.noreply.github.com>
2023-12-06 12:20:39 -0600

    Update To.lua

--- noisai/functions/To.lua
+++ noisai/functions/To.lua
@@ -5,30 +5,28 @@ return (function(Noisai)
 
 		if not to then to = self.At + 1 end;
 
-			local cur = self:Cur();
-			local nxt = self:Get(to);
-			
-			for _, s in pairs(cur:Sprites()) do
-				s.Transparency = 1
+		self.Moving:Fire(origin, to);
+
+		local cur = self:Cur();
+		local nxt = self:Get(to);
+
+		for _, s in pairs(cur:Sprites()) do
+			s.Transparency = 1
+		end
+
+		if nxt then
+			for _, s in pairs(nxt:Sprites()) do
+				s.Transparency = 0
 			end
-			
-			if nxt then
-				for _, s in pairs(nxt:Sprites()) do
-					s.Transparency = 0
-				end
-			end
+		end
 
-			cur.SFX:Play()
+		cur.SFX:Play()
 
-			if (self.At == #self.Path) then
-				self:Jumpscare()
-			else
-				self.At = to
-			end
-
-			self.Moving:Fire(origin, to);
-
-			return cur
+		if (self.At == #self.Path) then
+			self:Jumpscare()
+		else
+			self.At = to
+		end
 	end
 
 
